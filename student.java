7dfebb41e89559d28876e4012ed8e146e241865c
linjianxiang22 <104889689+linjianxiang22@users.noreply.github.com>
2022-05-04 16:56:33 +0800

    Update student.java

--- student.java
+++ student.java
@@ -1,10 +1,5 @@
-package qweqwe;
-
-public class student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("hello");
-	}
-
+import java.util.*
+class student{
+  private string name;
+  private string address;
 }
